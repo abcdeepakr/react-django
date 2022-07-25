9419d99a80237e94779b49a977c91621acc8a467
DeepakR-28 <awsmyo1234@gmail.com>
2022-07-25 16:11:31 +0530

    update

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -9,6 +9,7 @@ class Todo(models.Model):
   title = models.CharField(max_length=120)
   description = models.TextField()
   completed = models.BooleanField(default=False)
+  newField = models.TextField(default="test")
       
   def __str__(self):
     return self.title
